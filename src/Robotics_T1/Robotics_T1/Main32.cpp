91c1a9893968005a026bdf4a3ce2c1b313fecfac
João Barata Oliveira <jmdbo@hotmail.com>
2015-03-20 18:38:55 +0000

    Update multiple axis w/ read

--- src/Robotics_T1/Robotics_T1/Main32.cpp
+++ src/Robotics_T1/Robotics_T1/Main32.cpp
@@ -50,11 +50,27 @@ void move_one_axis_speed(TCommPort* Cp, int axis, int steps, int speed){
 
 	return;
 }
+//for (int i = 0; i < 6; i++)
+//{
+//	lsbStep[i] = steps[i] % 2;
+//	steps[i] = steps[i] >> 1;
+//}
+//char command[20] = { 0x0F, steps[0], steps[1], steps[2], steps[3], steps[4], steps[5],
+//lsbStep[0], lsbStep[1], lsbStep[2], lsbStep[3], lsbStep[4], lsbStep[5], 3 };
+//int lsbStep = 0;
+//for (int i = 5; i >= 0; i--)
+//{
+//	lsbStep = lsbStep + steps[i] % 2;
+//	steps[i] = steps[i] >> 1;
+//	if (i != 0)
+//		lsbStep << 1;
+//}
+//lsbStep = 1;
 
-void move_multiple_axis(TCommPort *Cp){
+void move_multiple_axis(TCommPort *Cp, int* steps){
 	int tam;
-	char command[20]= { 0x0F, 0x96, 0x96, 0x96, 0x96, 0x96, 0x96, 3 };
 
+	char command[20] = { 0x0F, steps[0], steps[1], steps[2], steps[3], steps[4], steps[5], 3 };
 	Cp->Enviar(command, 8, tam);
 	printf("\n%s...", Cp->GetMensagem());
 	Sleep(500);
@@ -63,6 +79,31 @@ void move_multiple_axis(TCommPort *Cp){
 	Cp->EsperarRecepcao();
 	Cp->Receber(Buff, 1, tam);
 	printf("\n%s...",Cp->GetMensagem());
+
+	return;
+}
+
+void move_multiple_axis_speed(TCommPort *Cp, int* steps, int* speed){
+	int tam;
+	char Buff[128];
+	char command1[20] = {0x47,3};
+	Cp->Enviar(command1, 2, tam);
+	Cp->EsperarRecepcao();
+	Cp->Receber(Buff, 8, tam);
+	for (int i = 0; i < 6; i++)
+	{
+		if (steps[i] == -1){
+			steps[i] = (unsigned char)Buff[i + 1];
+		}
+	}
+	char command[20] = { 0x0F, steps[0], steps[1], steps[2], steps[3], steps[4], steps[5], 3 };
+	Cp->Enviar(command, 8, tam);
+	printf("\n%s...", Cp->GetMensagem());
+	Sleep(500);
+	//robot wil respond with 15
+	Cp->EsperarRecepcao();
+	Cp->Receber(Buff, 1, tam);
+	printf("\n%s...", Cp->GetMensagem());
 
 	return;
 }
@@ -91,9 +132,16 @@ void robot_control_routine(TCommPort *port)
 				menu = 0; 
 				break;
 			case 2: 
-				move_multiple_axis(port);
+				int vector[6];
+				for (int i = 0; i < 6; i++)
+				{
+					vector[i] = -1;
+				}
+				move_multiple_axis_speed(port, vector, vector);
 				break;
-			case 3:scanf(" %d %d %d",&axis, &steps,&speed);
+			case 3:
+				printf("axis steps speed: ");
+				scanf(" %d %d %d",&axis, &steps,&speed);
 				move_one_axis_speed(port, axis, steps, speed);
 				break;
 			case 10: exit = FALSE;
