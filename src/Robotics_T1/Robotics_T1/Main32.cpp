a6476d6cbd0072d74c8f1278e3de0225c5352b30
Pedro Miguel dos Santos Martins <pmd.martins@campu.fct.unl.pt>
2015-03-21 21:13:34 +0000

    move_one_axis updated, mm_to_step and step_to_mm added.
    the degree function is working/tested with the move_one_axis function.
    Added step_to_mm and mm_to_step, but was not tested.

--- src/Robotics_T1/Robotics_T1/Main32.cpp
+++ src/Robotics_T1/Robotics_T1/Main32.cpp
@@ -36,19 +36,20 @@ void move_one_axis(TCommPort *Cp, int axis, int steps)
 }
 
 void move_one_axis_speed(TCommPort* Cp, int axis, int steps, int speed){
-	int tam;
-	char command[20] = { 0x08 + axis - 1, steps, speed, 3 };
+	if (steps == -1){
+		int tam;
+		char command[20] = { 0x08 + axis - 1, steps, speed, 3 };
 
-	Cp->Enviar(command, 4, tam);
-	printf("\n%s...", Cp->GetMensagem());
-	Sleep(500);
-	char Buff[128];
-	//robot will respond with 15
-	Cp->EsperarRecepcao();
-	Cp->Receber(Buff, 1, tam);
-	printf("\n%s...", Cp->GetMensagem());
+		Cp->Enviar(command, 4, tam);
+		printf("\n%s...", Cp->GetMensagem());
+		Sleep(500);
+		char Buff[128];
+		//robot will respond with 15
+		Cp->EsperarRecepcao();
+		Cp->Receber(Buff, 1, tam);
+		printf("\n%s...", Cp->GetMensagem());
 
-
+	}
 	return;
 }
 
@@ -69,28 +70,32 @@ void move_multiple_axis(TCommPort *Cp, int* steps){
 }
 
 void move_multiple_axis_speed(TCommPort *Cp, int* steps, int* speed){
-	int tam;
-	char Buff[128];
-	char command1[20] = { 0x47, 3 };
+	
+	int tam, count=0;
+	char Buff[128], command1[20] = { 0x47, 3 };
+	
 	Cp->Enviar(command1, 2, tam);
 	Cp->EsperarRecepcao();
 	Cp->Receber(Buff, 8, tam);
+	
 	for (int i = 0; i < 6; i++)
 	{
 		if (steps[i] == -1){
 			steps[i] = (unsigned char)Buff[i + 1];
+			count++;
 		}
 	}
-	char command[20] = { 0x0F, steps[0], steps[1], steps[2], steps[3], steps[4], steps[5], 3 };
-	Cp->Enviar(command, 8, tam);
-	printf("\n%s...", Cp->GetMensagem());
-	Sleep(500);
-	//robot wil respond with 15
-	Cp->EsperarRecepcao();
-	Cp->Receber(Buff, 1, tam);
-	printf("\n%s...", Cp->GetMensagem());
 
-	return;
+	if (count <= 6){
+		char command[20] = { 0x0F, steps[0], steps[1], steps[2], steps[3], steps[4], steps[5], 3 };
+		Cp->Enviar(command, 8, tam);
+		printf("\n%s...", Cp->GetMensagem());
+		Sleep(500);
+		//robot wil respond with 15
+		Cp->EsperarRecepcao();
+		Cp->Receber(Buff, 1, tam);
+		printf("\n%s...", Cp->GetMensagem());
+	}
 }
 
 int* all_motor_status(TCommPort *Cp ){
@@ -149,6 +154,17 @@ int degrees_to_steps(double degrees, int axis){
 	else return step;
 }
 
+int mm_to_steps(double distance){
+	int steps = 0;
+	double stepsf;
+
+	distance = distance + 60;
+	stepsf = distance / 0.2352941176470588;
+	steps = (int)stepsf + 0.5;
+
+	return steps;
+}
+
 double steps_to_degrees(int steps, int axis){
 	double degrees;
 	switch (axis)
@@ -179,6 +195,15 @@ double steps_to_degrees(int steps, int axis){
 	return degrees;
 }
 
+double stpes_to_mm(int steps){
+	double distance;
+
+	distance = steps*0.2352941176470588;
+	distance = distance - 60;
+
+	return distance;
+}
+
 //You should implement the other required operations here.
 
 void robot_control_routine(TCommPort *port)
@@ -186,14 +211,14 @@ void robot_control_routine(TCommPort *port)
 	//Create the control routine here with a menu that allows the user to choose
 	//which operation the robot should perform (e.g. move_one_axis, move_one_axis_speed...)
 	int menu = 0;
-	int axis = 0, steps = 0, speed = 0;
-	int steparray[6];
+	int axis = 0, steps = 0, speed = 0, degrees=0;
+	int steparray[6],speedarray[6];
 	bool exit = TRUE;
 
 	while (exit){
 
 		printf("\n**********Menu**********\n1-move_one_axis\n2-move_one_axis_speed");
-		printf("\n3-multiple axis\n4-multiple axis_speed");
+		printf("\n3-multiple axis\n4-multiple axis_speed\n");
 		scanf(" %d", &menu);
 
 		switch (menu)
@@ -201,8 +226,8 @@ void robot_control_routine(TCommPort *port)
 		case 1:
 			//move one axis
 			printf("axis steps: ");
-			scanf("%d %d", &axis, &steps);
-			move_one_axis(port, axis, steps);
+			scanf("%d %d", &axis, &degrees);
+			move_one_axis(port, axis, degrees_to_steps(degrees,axis));
 			break;
 		case 2:
 			//move one axis speed 
@@ -214,21 +239,20 @@ void robot_control_routine(TCommPort *port)
 			// move multiple axis
 			for (int i = 0; i < 6; i++)
 			{
-				printf("Axis %d step: ");
+				printf("Axis %d step: ",i+1);
 				scanf(" %d", steparray[i]);
 			}
 			move_multiple_axis(port, steparray);
 			break;
 		case 4:
 			//move multiple axis speed
-			printf("");
 			int vector[6];
 			for (int i = 0; i < 6; i++)
 			{
-				printf("Axis %d step: ",i+1);
-				scanf(" %d", &steparray[i]);
+				printf("Axis %d step speed: ",i+1);
+				scanf(" %d %d", &steparray[i],&speedarray[i]);
 			}
-			move_multiple_axis_speed(port, vector, vector);
+			move_multiple_axis_speed(port, steparray, speedarray);
 			break;
 		case 10: exit = FALSE;
 		case 0:
